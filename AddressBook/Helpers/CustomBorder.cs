4f5fd631a4c1c184d3b3eeb5bf641972bafe0604
Craig Lawyer <cjl1197@gmail.com>
2024-01-22 13:35:54 -0500

    added simple subclass for border

---
+++ AddressBook/Helpers/CustomBorder.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Shapes;
+namespace AddressBook.Helpers;
+
+public class CustomBorder : Border
+{
+    public CustomBorder()
+    {
+        StrokeThickness = 2;
+        HorizontalOptions = LayoutOptions.FillAndExpand;
+        StrokeShape = new RoundRectangle
+        {
+            CornerRadius = new CornerRadius(5)
+
+        };
+        Margin = new Thickness(45, 5);
+    }
+
+}
--- AddressBook/obj/Debug/net8.0-android/AddressBook.AssemblyInfo.cs
+++ AddressBook/obj/Debug/net8.0-android/AddressBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AddressBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e5dfd401e962555f69ad664b99e6ecd9e61fde7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d4b26576e55ba1cb56876d22d986bb8462cfc1c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("AddressBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AddressBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
